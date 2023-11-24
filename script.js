d591d07b48230d6070d0d093a4a794e30dd8ab2a
Andrieli Cornelsen <149266258+andrielicornelsen@users.noreply.github.com>
2023-11-24 18:13:36 -0300

    Conversor de Moedas
    Projeto criado na Imersão Dev - Alura - JavaScript. O objetivo é entregar um conversor de moedas, Ex: Valor em Real para valor em Dólar!

---
+++ script.js
@@ -0,0 +1,9 @@
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.32;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+var textName = "Olá Andrieli o valor em real é";
+
+alert(textName + " R$" + valorEmReal);
